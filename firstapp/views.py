4495a276e9951d5c65d814cea80cc6de4df7696f
GGoReb <seorab@naver.com>
2022-01-17 11:57:03 +0900

    views.py index 등 주소 3개 생성, app urls 분리

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index1(request):
+    return HttpResponse('<u>Hello</u><hr>Bye')
+
+def index2(request):
+    return HttpResponse('<u>Hi</u>')
+
+def main(request):
+    return HttpResponse('<u>Main</u>')
